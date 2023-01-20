68d92a9c9be24e6f75633e82e9ff0ee49a1fcfab
Daniel Silveira <murtdaniel@gmail.com>
2023-01-20 17:31:34 -0300

    Sistema Bancário [Documentando projeto]

--- Conta.py
+++ Conta.py
@@ -31,17 +31,6 @@ class ContaPoupanca(Conta):
         self.saldo -= valor
 
 
-#class ContaCorrente(Conta):
-#    def __init__(self, agencia, conta, saldo, limite=100):
-#        super().__init__(agencia, conta, saldo)
-#        self.limite = limite
-
-#    def sacar(self, valor):
-#        if (self.saldo + self.limite) < valor:
-#            print('Saldo insuficiente')
-#            return
-
-#        self.saldo -= valor
 
 
 
